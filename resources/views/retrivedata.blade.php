a9ab105aa1a3023f1ea454770a5199d639a30d07
Shahzaib Mehfooz <shahzaibmehfooz86@gmail.com>
2020-10-07 17:27:00 +0500

    return to change id

--- resources/views/retrivedata.blade.php
+++ resources/views/retrivedata.blade.php
@@ -321,7 +321,7 @@
                   <table class="table table-hover text-nowrap">
                     <thead>
                       <tr>
-                        <th>I</th>
+                        <th>ID</th>
                         <th>UserName</th>
                         <th>Gender</th>
                         <th>Manage</th>
